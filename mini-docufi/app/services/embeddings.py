669e5cd34db41a7684e8be0c6ae9ad70b220bb5e
Ignacio Rigoni <ignacio.rigoni@gmail.com>
2025-09-12 20:10:51 -0300

    modified to show progress bar

--- mini-docufi/app/services/embeddings.py
+++ mini-docufi/app/services/embeddings.py
@@ -4,4 +4,7 @@ model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def generate_embeddings(texts: list[str]) -> list[list[float]]:
     """Generates embeddings for a list of texts."""
-    return model.encode(texts).tolist()
+    return model.encode(
+        texts,
+        show_progress_bar=True,
+    ).tolist()
